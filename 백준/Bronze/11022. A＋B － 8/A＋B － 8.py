2e17d63471e4b935d588fe0544e61a8386266d60
Jang KyuJin <106062329+kj021@users.noreply.github.com>
2023-01-01 17:16:52 +0900

    [Bronze V] Title: A+B - 8, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -1,4 +1,4 @@
-t = int(input())
-for x in range(1, t+1): 
-    a, b = map(int, input().split())
-    print(f'Case #{x}: {a} + {b} = {a+b}')
+T = int(input())
+for i in range(1, T+1):
+    A, B = map(int, input().split())
+    print("Case #" + str(i) + ":", A, "+", B, "=", A+B)
