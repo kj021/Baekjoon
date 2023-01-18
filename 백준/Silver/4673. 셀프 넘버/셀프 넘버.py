ae603097747863c983425bfae3bfe211a06b2505
Jang KyuJin <106062329+kj021@users.noreply.github.com>
2023-01-18 18:33:59 +0900

    [Silver V] Title: 셀프 넘버, Time: 108 ms, Memory: 31196 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -0,0 +1,12 @@
+numbers = list(range(1, 10_001))
+remove_list = []  
+for num in numbers :
+    for n in str(num):
+        num += int(n) 
+    if num <= 10_000:  
+        remove_list.append(num) 
+
+for remove_num in set(remove_list) : 
+    numbers.remove(remove_num)
+for self_num in numbers :  
+    print(self_num)
