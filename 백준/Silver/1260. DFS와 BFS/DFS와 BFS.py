197beabc1d44cb990bda2fae300d4c29d66f9e59
Jang KyuJin <106062329+kj021@users.noreply.github.com>
2023-06-01 10:31:55 +0900

    [Silver II] Title: DFS와 BFS, Time: 540 ms, Memory: 39516 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+N, M, V = map(int, input().split())
+
+graph = [[False] * (N + 1) for _ in range(N + 1)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a][b] = True
+    graph[b][a] = True
+
+visited1 = [False] * (N + 1)  # dfs의 방문기록
+visited2 = [False] * (N + 1)  # bfs의 방문기록
+
+
+def bfs(V):
+    q = deque([V])  # pop메서드의 시간복잡도가 낮은 덱 내장 메서드를 이용한다
+    visited2[V] = True  # 해당 V 값을 방문처리
+    while q:  # q가 빌때까지 돈다.
+        V = q.popleft()  # 큐에 있는 첫번째 값 꺼낸다.
+        print(V, end=" ")  # 해당 값 출력
+        for i in range(1, N + 1):  # 1부터 N까지 돈다
+            if not visited2[i] and graph[V][i]:  # 만약 해당 i값을 방문하지 않았고 V와 연결이 되어 있다면
+                q.append(i)  # 그 i 값을 추가
+                visited2[i] = True  # i 값을 방문처리
+
+
+def dfs(V):
+    visited1[V] = True  # 해당 V값 방문처리
+    print(V, end=" ")
+    for i in range(1, N + 1):
+        if not visited1[i] and graph[V][i]:  # 만약 i값을 방문하지 않았고 V와 연결이 되어 있다면
+            dfs(i)  # 해당 i 값으로 dfs를 돈다.(더 깊이 탐색)
+
+
+dfs(V)
+print()
+bfs(V)
